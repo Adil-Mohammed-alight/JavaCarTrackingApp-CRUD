6b530af8d3666f0a33442bf6c3c5e09d9ed0c9e9
adil <adiljamal88@gmail.com>
2023-02-09 23:47:13 +0530

    second commit

--- crud-car/src/main/java/crudcar/CrudCarApplication.java
+++ crud-car/src/main/java/crudcar/CrudCarApplication.java
@@ -2,6 +2,7 @@ package crudcar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudCarApplication {
@@ -10,5 +11,6 @@ public class CrudCarApplication {
 
 		SpringApplication.run(CrudCarApplication.class, args);
 	}
+
 
 }
